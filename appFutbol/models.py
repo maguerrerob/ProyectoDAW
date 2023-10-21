1160b7b8a95586f7d4223b17d23d8dc9e67cca30
Manuel Alejandro <manuel.guerrero.boza.al@iespoligonosur.org>
2023-10-21 18:51:49 +0200

    reestructuracion de modelos y creacion de relaciones

--- appFutbol/models.py
+++ appFutbol/models.py
@@ -11,26 +11,73 @@ class Usuario(models.Model):
     nivel = models.FloatField(default=0.0, db_column="puntos_usuario")
     telefono = models.CharField(max_length=9)
 
+
+class Recinto(models.Model):
+    nombre = models.TextField()
+    ubicacion = models.TextField()
+    telefono = models.CharField(max_length=9)
+    #--------Relaciones--------
+
+
+class Reserva(models.Model):
+    id = models.IntegerField(unique=True)
+    ESTADO = [
+        ("F", "Completo"),
+        ("A", "Disponible")
+    ]
+    estado = models.CharField(max_length=1, choices=ESTADO)
+    TIPO = [
+        ("Pr", "Privada"),
+        ("Pu", "Pública")
+    ]
+    tipo = models.CharField(max_length=2, choices=TIPO)
+    n_jugadores = models.IntegerField()
+    #--------Relaciones--------
+    creador = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    campo = models.ForeignKey(Recinto, on_delete=models.CASCADE)
+
+
+class Partido(models.Model):
+    id = models.IntegerField(unique=True)
+    ESTILO = [
+        (5, "Fútbol sala"),
+        (7, "Fútbol 7"),
+        (11, "Fútbol 11"),
+    ]
+    estilo = models.CharField(max_length=1, choices=ESTILO)
+    #--------Relaciones--------
+    usuarios_jugadores = models.ManyToManyField(Usuario, through="Jugadores_partido")
+    juego = models.OneToOneField(Reserva, on_delete=models.CASCADE)
+
+
+class Jugadores_partido(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    partido = models.ForeignKey(Partido, on_delete=models.CASCADE)
+    GANADOR = [
+        ("S", "Sí"),
+        ("N","No")
+    ]
+    ganar = models.CharField(max_length=1, choices=GANADOR)
+
+
+class Torneo(models.Model):
+    pass 
+
+
 class Equipo(models.Model):
     LOCALIA = [
         ("LO", "Local"),
         ("VI", "Visitante")
     ]
     localia = models.CharField(max_length=2, choices=LOCALIA)
-    ESTILO = [
-        (5, "Fútbol sala"),
-        (7, "Fútbol 7"),
-        (11, "Fútbol 11"),
-    ]
-    estilo = models.CharField(max_length=2, choices=ESTILO)
 
-class Partido(models.Model):
-    id = models.IntegerField(unique=True)
-    
 
 class Resultado(models.Model):
     goles_local = models.IntegerField(verbose_name="Goles local", null=False)
     goles_visitante = models.IntegerField(verbose_name="Goles visitante", null=False)
+    #--------Relaciones--------
+    partido = models.OneToOneField(Partido, on_delete=models.CASCADE)
+
 
 class DatosUsuario(models.Model):
     descripcion = models.TextField()
@@ -41,4 +88,11 @@ class DatosUsuario(models.Model):
         ("STR", "Delantero")
     ]
     posicion = models.CharField(max_length=2, choices=POSICION)
-    resultados = models.ForeignKey(Resultado)
+    partidos = models.ManyToManyField(Partido)
+    ubicacion = models.TextField()
+    #--------Relaciones--------
+    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
+
+
+class Posts(models.Model):
+    pass
