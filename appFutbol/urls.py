77196fffc8c345fbe3d181bb318f2e567f89910f
Manuel Alejandro <manuel.guerrero.boza.al@iespoligonosur.org>
2023-11-05 04:19:32 +0100

    3 query sets

--- appFutbol/urls.py
+++ appFutbol/urls.py
@@ -4,5 +4,6 @@ from .import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("usuarios", views.usuarios, name="lista_usuarios"),
-    path("usuario_por_posicion/<str:pos>", views.buscar_usuario, name="buscar_usuario")
+    path("usuario_por_posicion/<str:pos>", views.buscar_usuario, name="buscar_usuario"),
+    path("reservas_usuario/<int:id_usuario>", views.reservas_usuario, name="reservas_usuario")
 ]
--- appFutbol/views.py
+++ appFutbol/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Usuario
+from .models import Usuario, Reserva
 from django.db.models import Q, Prefetch
 
 # Create your views here.
@@ -27,11 +27,19 @@ def buscar_usuario(request, pos):
     usuarios = QSusuarios.filter(datos_usuario__posicion__contains=pos).all()
     return render(request, "usuarios/usuario_posicion.html", {"usuarios":usuarios})
 
-#3. Mostrar los usuarios que hayan creado más X posts
+'''DUDA. Mostrar los usuarios que hayan creado más X posts
 
 def usuarios_post(request):
     QSusuarios = Usuario.objects.prefetch_related(Prefetch("creador_reserva"),
                                                   Prefetch("datos_usuario"), 
                                                   Prefetch("creador_post"),
                                                   Prefetch("creador_reserva__campo_reservado"))
-    #usuarios = QSusuarios.filter(datos_usuario__)
+    #usuarios = QSusuarios.filter(datos_usuario__)'''
+
+#3. Obtener las reservas realizadas de X usuario
+
+def reservas_usuario(request, id_usuario):
+    usuario_mostrar = Usuario.objects.get(id=id_usuario)
+    QSreserva = Reserva.objects.select_related("creador").select_related("campo_reservado")
+    reservas = QSreserva.filter(creador=id_usuario).all()
+    return render(request, "reservas/reservas_usuarios.html", {"usuario":usuario_mostrar, "reservas":reservas})
